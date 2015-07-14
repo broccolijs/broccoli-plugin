a1c8791ee6b34ac770e48a33ce317aa2b9e8bca4
Jo Liss <joliss42@gmail.com>
2015-07-14 22:33:00 +0000

    Indent

--- test/test.js
+++ test/test.js
@@ -104,41 +104,41 @@ describe('unit tests', function() {
     expect(new NoopPlugin([], { annotation: 'some note' }) + '').to.equal('[NoopPlugin: some note]')
   })
 
-describe('usage errors', function() {
-  it('requires the base constructor to be called (super)', function() {
-    TestPlugin.prototype = Object.create(Plugin.prototype)
-    TestPlugin.prototype.constructor = TestPlugin
-    function TestPlugin() { /* no Plugin.apply(this, arguments) here */ }
-    TestPlugin.prototype.build = function() {}
+  describe('usage errors', function() {
+    it('requires the base constructor to be called (super)', function() {
+      TestPlugin.prototype = Object.create(Plugin.prototype)
+      TestPlugin.prototype.constructor = TestPlugin
+      function TestPlugin() { /* no Plugin.apply(this, arguments) here */ }
+      TestPlugin.prototype.build = function() {}
 
-    return expect(build(new Builder_0_16_3(new TestPlugin)))
-      .to.be.rejectedWith(Error, /must call the superclass constructor/)
-  })
+      return expect(build(new Builder_0_16_3(new TestPlugin)))
+        .to.be.rejectedWith(Error, /must call the superclass constructor/)
+    })
 
-  it('disallows overriding read, cleanup, and rebuild', function() {
-    var prohibitedNames = ['read', 'rebuild', 'cleanup']
-    for (var i = 0; i < prohibitedNames.length; i++) {
-      var BadPlugin = function BadPlugin() {
-        Plugin.apply(this, arguments)
+    it('disallows overriding read, cleanup, and rebuild', function() {
+      var prohibitedNames = ['read', 'rebuild', 'cleanup']
+      for (var i = 0; i < prohibitedNames.length; i++) {
+        var BadPlugin = function BadPlugin() {
+          Plugin.apply(this, arguments)
+        }
+        BadPlugin.prototype = Object.create(Plugin.prototype)
+        BadPlugin.prototype.constructor = BadPlugin
+        BadPlugin.prototype.build = function() {}
+        BadPlugin.prototype[prohibitedNames[i]] = function() {}
+
+        expect(function() { new BadPlugin([]) })
+          .to.throw(/For compatibility, plugins must not define/)
       }
-      BadPlugin.prototype = Object.create(Plugin.prototype)
-      BadPlugin.prototype.constructor = BadPlugin
-      BadPlugin.prototype.build = function() {}
-      BadPlugin.prototype[prohibitedNames[i]] = function() {}
+    })
 
-      expect(function() { new BadPlugin([]) })
-        .to.throw(/For compatibility, plugins must not define/)
-    }
-  })
+    it('checks that the inputNodes argument is an array', function() {
+      expect(function() { new AnnotatingPlugin('notAnArray') })
+        .to.throw(/Expected an array/)
+    })
 
-  it('checks that the inputNodes argument is an array', function() {
-    expect(function() { new AnnotatingPlugin('notAnArray') })
-      .to.throw(/Expected an array/)
+    it('provides a helpful error message on missing `new`', function() {
+      expect(function() { AnnotatingPlugin([]) })
+        .to.throw(/Missing `new`/)
+    })
   })
-
-  it('provides a helpful error message on missing `new`', function() {
-    expect(function() { AnnotatingPlugin([]) })
-      .to.throw(/Missing `new`/)
-  })
-})
 })
